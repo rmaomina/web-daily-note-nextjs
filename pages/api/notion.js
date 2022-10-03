eb901db5e7647618c88e1fd46962a0f305b3f14c
mina <rmaomina@gmail.com>
2022-10-03 21:38:06 +0900

    feat: notion api 연결

---
+++ pages/api/notion.js
@@ -0,0 +1,10 @@
+const { Client } = require('@notionhq/client')
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY })
+
+export default async (req, res) => {
+	const response = await notion.databases.retrieve({ 
+    database_id: process.env.NOTION_DATABASE_ID 
+  })
+	res.status(200).json({ response })
+}
